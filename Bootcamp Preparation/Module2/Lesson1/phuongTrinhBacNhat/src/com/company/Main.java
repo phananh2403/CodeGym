6728d36dad573d867ef8ca54251325b11471a3e4
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-07-27 09:24:10 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson1/phuongTrinhBacNhat/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c', please enter constants:");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+        System.out.println("c: ");
+        double c = scanner.nextDouble();
+
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == c) {
+                System.out.print("The solution is all x!");
+            } else {
+                System.out.print("No solution!");
+            }
+        }
+
+    }
+}
