f9db8a52b859fee4b6b108af4f466a68641d2a79
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-07-27 08:57:33 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson1/toanTu/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
