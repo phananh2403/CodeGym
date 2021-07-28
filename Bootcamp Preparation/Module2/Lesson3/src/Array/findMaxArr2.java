75b74575d6624c6b06b67ab5e8d1fe4bca087586
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-07-28 17:21:20 +0700

    tim max mang 2 chieu

---
+++ Bootcamp Preparation/Module2/Lesson3/src/Array/findMaxArr2.java
@@ -0,0 +1,39 @@
+package Array;
+
+import java.util.Scanner;
+
+public class findMaxArr2 {
+    public static void main(String[] args) {
+        int m, n;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập vào số dòng của ma trận: ");
+        m = scanner.nextInt();
+        System.out.println("Nhập vào số cột của ma trận: ");
+        n = scanner.nextInt();
+
+        int A[][] = new int[m][n];
+
+        System.out.println("Nhập các phần tử cho ma trận: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("A[" + i + "]["+ j + "] = ");
+                A[i][j] = scanner.nextInt();
+            }
+        }
+        
+        int max = A[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (max < A[i][j]) {
+                    max = A[i][j];
+                }
+            }
+        }
+
+        System.out.println("Phần tử lớn nhất trong ma trận = " + max);
+
+    }
+}
