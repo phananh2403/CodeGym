59f7d0ce8360b907f1e6e40fbc0acd52ea821e4e
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-03 18:14:31 +0700

    đa hình 1

---
+++ Bootcamp Preparation/Module2/Lesson7/src/Resizeable/Resizeable.java
@@ -0,0 +1,5 @@
+package Resizeable;
+
+public interface Resizeable {
+    public abstract void resize(double percent);
+}
---
+++ Bootcamp Preparation/Module2/Lesson7/src/Resizeable/Square.java
@@ -0,0 +1,44 @@
+package Resizeable;
+
+public class Square extends Shape {
+    private double side;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(double side, String color, boolean filled) {
+        super(color,filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString()
+                + ", Area=" +
+                + getArea();
+    }
+
+    double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public void resize(double percent) {
+        this.side *= (percent / 200);
+    }
+}
