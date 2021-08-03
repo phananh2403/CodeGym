48807a2324c9f0336279e3c6b7c9e5f5a767322d
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-03 17:10:22 +0700

    Add files via upload

--- Bootcamp Preparation/Module2/Lesson7/src/Comparable/ComparableCircleTest.java
+++ Bootcamp Preparation/Module2/Lesson7/src/Comparable/ComparableCircleTest.java
@@ -1,6 +1,7 @@
 package Comparable;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ComparableCircleTest {
     public static void main(String[] args) {
@@ -14,6 +15,7 @@ public class ComparableCircleTest {
             System.out.println(circle);
         }
 
+        Comparator circleComparator = new CircleComparator();
         Arrays.sort(circles);
 
         System.out.println("After-sorted:");
