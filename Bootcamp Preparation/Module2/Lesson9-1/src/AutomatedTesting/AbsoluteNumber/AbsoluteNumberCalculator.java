9e3fece571e84b96b94efc4cb056931d83c099f6
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-05 15:14:01 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson9-1/src/AutomatedTesting/AbsoluteNumber/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package AutomatedTesting.AbsoluteNumber;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
