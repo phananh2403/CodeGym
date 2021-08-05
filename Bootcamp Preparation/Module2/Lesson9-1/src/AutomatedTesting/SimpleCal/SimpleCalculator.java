0dd3f81eb4c7455695920262ac70b6584366748f
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-05 14:55:46 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson9-1/src/AutomatedTesting/SimpleCal/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package AutomatedTesting.SimpleCal;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
