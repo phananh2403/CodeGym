e45d4ce2f349b2b26f4fbb930d98d69faddee8b9
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-10 11:50:58 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson11/src/StackQueue/reverse/ReverseNumString.java
@@ -0,0 +1,20 @@
+package StackQueue.reverse;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class ReverseNumString {
+    public static void main(String args[]) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] myArray = {23, 93, 56, 92, 39};
+        int size = myArray.length;
+        for (int i = 0; i < size; i++) {
+            stack.push(myArray[i]);
+        }
+        int[] reverseArray = new int[size];
+        for (int i = 0; i < size; i++) {
+            reverseArray[i] = stack.pop();
+        }
+        System.out.println("Reversed array is :" + Arrays.toString(reverseArray));
+    }
+}
