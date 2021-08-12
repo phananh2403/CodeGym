ed4dc9d086229abc8475208571d4099a6b5b5168
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-12 14:04:12 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson12/src/TreeAndMap/InsertBST/AbstractTree.java
@@ -0,0 +1,7 @@
+package TreeAndMap.InsertBST;
+
+public abstract class AbstractTree<E> implements Tree<E>{
+    @Override /* Inorder traversal from the root*/
+    public void inorder() {
+    }
+}
---
+++ Bootcamp Preparation/Module2/Lesson12/src/TreeAndMap/InsertBST/TestBST.java
@@ -0,0 +1,19 @@
+package TreeAndMap.InsertBST;
+
+public class TestBST {
+    public static void main(String[] args) {
+        //create a BST
+        BST<String> tree = new BST<>();
+        tree.insert("George");
+        tree.insert("Michael");
+        tree.insert("Tom");
+        tree.insert("Adam");
+        tree.insert("Jones");
+        tree.insert("Peter");
+        tree.insert("Daniel");
+        //traverse tree
+        System.out.println("Inorder (sorted): ");
+        tree.inorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
