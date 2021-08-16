595d3642210df77d343b6f1cde4f2d3fd0ddd47a
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-16 09:51:09 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson14/src/Sort/InsertionSort.java
@@ -0,0 +1,39 @@
+package Sort;
+
+public class InsertionSort {
+    // Phương thức sắp xếp chèn
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Di chuyển các phần tử của arr [0 ... i - 1], lớn hơn key
+            // đến một vị trí trước vị trí hiện tại của chúng
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // In các phần tử của mảng
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 12, 11, 13, 5, 6 };
+        System.out.println("Mảng ban đầu:");
+        printArray(arr);
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        System.out.println("Mảng sau khi sắp xếp:");
+        printArray(arr);
+    }
+}
