a91c8c66c411d771dc2644fd3f74ec8c3f3e74d2
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-17 13:53:51 +0700

    Add files via upload

--- Bootcamp Preparation/Module2/QuanLyHocVien/src/Manager/StudentManager.java
+++ Bootcamp Preparation/Module2/QuanLyHocVien/src/Manager/StudentManager.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StudentManager {
     public static Scanner scanner = new Scanner(System.in);
@@ -35,6 +37,23 @@ public class StudentManager {
         int id = (studentList.size() > 0) ? (studentList.size() + 1) : 1;
         System.out.println("Student's ID = " + id);
         String name = inputName();
+//        try {
+//            Integer.parseInt(name);
+//            System.out.println("Name is number");
+//        } catch (Exception e){
+//            System.out.println("Name is string");
+//        }
+
+        //Kiểm tra đầu vào của tên sinh viên.
+        Pattern pattern = Pattern.compile("\\d*");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()) {
+            System.out.println("Name Is a Number");
+        } else {
+            System.out.println("Name Not a Number");
+        }
+
+
         float firstScore = inputFirst();
         float secondScore = inputSecond();
         float thirdScore = inputThird();
