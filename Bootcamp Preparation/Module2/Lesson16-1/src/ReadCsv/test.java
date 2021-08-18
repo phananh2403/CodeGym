a581daf83caa421d01db23efad4523c031bc45eb
Nguyễn Phan Anh <69679132+phananh2403@users.noreply.github.com>
2021-08-18 15:07:52 +0700

    Add files via upload

---
+++ Bootcamp Preparation/Module2/Lesson16-1/src/ReadCsv/test.java
@@ -0,0 +1,51 @@
+package ReadCsv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+
+        BufferedReader br = null;
+        try {
+            String line;
+            br = new BufferedReader(new FileReader("D:\\CodeGym\\CodeGym\\Bootcamp Preparation\\Module2\\Lesson16-1\\src\\ReadCsv\\data.csv"));
+
+            while ((line = br.readLine()) != null) {
+                printCountry(parseCsvLine(line));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static List<String> parseCsvLine(String csvLine) {
+        List<String> result = new ArrayList<>();
+        if (csvLine != null) {
+            String[] splitData = csvLine.split(",");
+            result.addAll(Arrays.asList(splitData));
+        }
+        return result;
+    }
+
+    private static void printCountry(List<String> country) {
+        System.out.println(
+                "Country [id= "
+                        + country.get(0)
+                        + ", code= " + country.get(1)
+                        + " , name=" + country.get(2)
+                        + "]");
+    }
+}
